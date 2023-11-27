0eeb359ad77966a5ebfe511fb18533a218113fff
samuelhabib <samuel.habib31@gmail.com>
2023-11-26 20:28:48 -0500

    Revert "Merge pull request #11 from Akhamis01/janakan/init"
    This reverts commit ebeff33c38df888a35ec5a25e3e34823f896a4ab, reversing
    changes made to 0c2d1d2a1b53ebece8464fcf91f5395dba4eee07.

--- frontend/src/components/Feedback/Feedback.js
+++ frontend/src/components/Feedback/Feedback.js
@@ -1,68 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar';
-import './Feedback.css';
+import NavBar from '../NavBar/NavBar'
+import "./Feedback.css";
 
 export default function Feedback() {
-   const [feedback, setFeedback] = useState([]);
-   const [activeFeedback, setActiveFeedback] = useState([]);
-   const [userType, setUserType] = useState('');
-   const navigate = useNavigate();
+    const [feedback, setFeedback] = useState([]);
+    const [activeFeedback, setActiveFeedback] = useState([]);
+    const [userType, setUserType] = useState('');
+    const navigate = useNavigate();
 
-   useEffect(() => {
-      fetch('/getFeedback')
-         .then((res) => res.json())
-         .then((data) => {
+    useEffect(() => {
+        fetch("/getFeedback").then(res => res.json()).then(data => {
             setFeedback(data);
             setActiveFeedback(data[0]);
-            console.log(data);
-         });
+            console.log(data)
+        });
 
-      fetch('/getusertype')
-         .then((res) => res.json())
-         .then((data) => {
+        fetch("/getusertype").then(res => res.json()).then(data => {
             setUserType(data['type']);
-         });
-   }, []);
+        });
+    }, [])
 
-   const updateActiveFeedback = (data) => {
-      setActiveFeedback(data);
-   };
+    const updateActiveFeedback = (data) => {
+        setActiveFeedback(data);
+    }
 
-   return (
-      <div className="order-main-bg">
-         <NavBar userType={userType} />
-         <div className="main-body d-flex justify-content-center align-items-center h-100">
-            {/* <div className="main-header"> */}
-            <div className="container">
-               <h1>Feedback</h1>
-               <div className="d-flex flex-row">
-                  <div class="list-group mr-3">
-                     {
-                        // Todo: active class
-                        feedback.map((feed) => (
-                           <a
-                              onClick={(event) => updateActiveFeedback(feed)}
-                              href="#"
-                              className={
-                                 'list-group-item list-group-item-action ' +
-                                 (feed == activeFeedback ? 'active' : '')
-                              }
-                              aria-current="true"
-                           >
-                              {feed.name}
-                           </a>
-                        ))
-                     }
-                  </div>
-                  <div class="ml-3">
-                     <p>Name: {activeFeedback.name}</p>
-                     <p>Email: {activeFeedback.email}</p>
-                     <p>Message: {activeFeedback.message}</p>
-                  </div>
-               </div>
+    return (
+        
+        <div className="order-main-bg">
+            <NavBar userType={userType}/>
+            <div className="main-body d-flex justify-content-center align-items-center h-100">
+                {/* <div className="main-header"> */}
+                <div className="container">
+                    <h1>Feedback</h1>
+                    <div className="d-flex flex-row">
+                        <div class="list-group mr-3">
+                            {
+                                // Todo: active class
+                                feedback.map((feed) => (
+                                    <a onClick={(event) => updateActiveFeedback(feed)}href="#" className={"list-group-item list-group-item-action " + (feed == activeFeedback ? 'active' : '')} aria-current="true">{feed.name}</a>
+                                ))
+                            }
+                        </div>
+                        <div class='ml-3'>
+                            <p>Name: {activeFeedback.name}</p>
+                            <p>Email: {activeFeedback.email}</p>
+                            <p>Message: {activeFeedback.message}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   );
+        </div>
+    );
 }
--- frontend/src/components/Verification-Pending/Verification-Pending.js
+++ frontend/src/components/Verification-Pending/Verification-Pending.js
@@ -3,29 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import './Verification-Pending.css';
 
 const VerificationPending = () => {
-   return (
-      <div className="order-main-bg">
-         <div className="verification-body">
-            <div className="verification-form" style={{ width: 1000 }}>
-               <h1 id="verification-top" style={{ color: 'white' }}>
-                  Verification Pending
-               </h1>
-               <p style={{ color: 'white' }}>
-                  A message has been sent to the email you provided. <br></br>
-                  Please follow the instructions to complete verification, then
-                  try logging in again.
-               </p>
-               <a
-                  id="logins-btn"
-                  className="btn btn-primary btn-rounded"
-                  href="/login"
-               >
-                  Back to Login
-               </a>
+
+    return (
+        <div className="order-main-bg">
+            <div className="verification-body">
+                <div className="verification-form" style={{width: 1000, }}>
+                    <h1  id="verification-top" style={{ color: 'white' }}>Verification Pending</h1>
+                    <p style={{ color: 'white' }}>A message has been sent to the email you provided. <br></br>
+                    Please follow the instructions to complete verification, then try logging in again.</p>
+                    <a id="logins-btn" className="btn btn-primary btn-rounded" href="/login">Back to Login</a>
+                </div>
             </div>
-         </div>
-      </div>
-   );
+        </div>
+    );
 };
 
 export default VerificationPending;
